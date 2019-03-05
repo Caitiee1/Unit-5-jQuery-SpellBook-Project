f89519dbd344b7dc76fe3924764e2694fd15469d
Caitiee1 <43577075+Caitiee1@users.noreply.github.com>
2019-03-05 09:24:45 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".button1").click(function(){
+$(".upside-down").fadeOut(3000);
+});
+
+
+
+//Spell #2
+
+$(".button2 ").click(function(){
+$(".elephant").fadeOut(20000);
+});
+
+
+
+//Spell #3
+
+$(".button3").click(function(){
+$(".escape").fadeOut(3000); 
+});
+
+
+
